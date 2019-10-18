9ff39fbd74361751a9cd293f5bc6649e93eb8edc
dhskrystallek <37992132+dhskrystallek@users.noreply.github.com>
2019-10-18 14:51:53 +0800

    Updated Krystal Lek

--- t01primeshcflcm_factors1.py
+++ t01primeshcflcm_factors1.py
@@ -2,8 +2,12 @@
 # factor: an integer which when multiplied with another integer, results in the product 18. 
 # Hence when 18 is divided by this number, the dividend is an integer and there are no remainders.
 
+# num is 18.
 num = 18
 
+# execute a loop
 for i in range(1, num+1):  
   if num % i == 0:
+    
+# print the values in the bracket
     print(i, end=' ')
